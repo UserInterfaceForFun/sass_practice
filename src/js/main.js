167a497ac68504a317dbe9712d77b7eed4013a3f
鄭佳旻 <jamie.cheng@kkday.com>
2018-07-13 12:43:32 +0800

    交作業囉

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log('main.js');
+
+$(function(){
+    $('.cd-prpl').on('click',function(){
+        $(this).parent().toggleClass('ss-prpl active')
+    })
+    $('.cd-red').on('click',function(){
+        $(this).parent().toggleClass('ss-red active')
+    })
+    $('.cd-org').on('click',function(){
+        $(this).parent().toggleClass('ss-org active')
+    })
+    $('.cd-ylw').on('click',function(){
+        $(this).parent().toggleClass('ss-ylw active')
+    })
+});
+	
